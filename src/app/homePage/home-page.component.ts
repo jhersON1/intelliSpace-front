971d1ad8e91f56e9940cdda24f198f5796488446
FaduaJ <faduaveliz@gmail.com>
2024-02-06 21:51:49 -0400

    Create navbar component

--- src/app/homePage/home-page.component.ts
+++ src/app/homePage/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
