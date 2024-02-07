b75933778e876da5fd86a348187db94f79877b1e
jherson rodriguez <50873601+jhersON1@users.noreply.github.com>
2024-02-07 19:15:28 -0400

    Revert "Create navbar component"

--- src/app/homePage/home-page.component.ts
+++ src/app/homePage/home-page.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../components/navbar/navbar.component';
-import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export default class HomePageComponent {
-  banner = "assets/banner/banner.png"
+
 }
