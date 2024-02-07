e3838c9bfed7db0429e52bb06d5e429b25e441d0
FaduaJ <faduaveliz@gmail.com>
2024-02-07 10:20:25 -0400

    Create footer component

--- src/app/homePage/home-page.component.ts
+++ src/app/homePage/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
