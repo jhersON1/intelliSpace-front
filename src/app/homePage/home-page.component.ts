5acd0cfd19091d3287ae3300c44c5631e6f93c1e
FaduaJ <faduaveliz@gmail.com>
2024-02-07 20:48:06 -0400

    navbar-footer

--- src/app/homePage/home-page.component.ts
+++ src/app/homePage/home-page.component.ts
@@ -1,11 +1,15 @@
-import { Component, signal } from '@angular/core';
-import ProductCardComponent from "./pages/product-card/product-card.component";
+import { Component, signal, HostListener } from '@angular/core';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import ProductCardComponent from './pages/product-card/product-card.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    ProductCardComponent
+    ProductCardComponent,
+    NavbarComponent, 
+    FooterComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
@@ -18,4 +22,17 @@ export default class HomePageComponent {
     {name: 'Table wood 3', company: 'Company name 3', image: 'assets/product-card/table-wood3.png'},
     {name: 'Table wood 4', company: 'Company name 4', image: 'assets/product-card/table-wood4.png'},
   ])
+
+  banner = "assets/banner/banner.png"
+  navbarBackgroundColor = 'rgba(255, 255, 255, 0.374)';
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any) {
+    const scrollPosition = window.pageYOffset;
+    if (scrollPosition > 790) {
+      this.navbarBackgroundColor = 'rgba(255, 255, 255, 1)'; 
+    } else {
+      this.navbarBackgroundColor = 'rgba(255, 255, 255, 0.374)'; 
+    }
+  }
 }
